ba57a0afd4ca49fbd5fb5bda3173e6b45e979c11
Sathors <sathors@free.fr>
2022-03-31 17:44:19 -0500

    Make it work in its primary version.

--- check_github_trending.sh
+++ check_github_trending.sh
@@ -1,22 +1,23 @@
 #! /bin/bash
 
+set -e
+
 # Features:
-# When passed a list of github repos, find out the ones I have never checked and the ones that I have checked in the past, but the last time was more than 3 months ago.
+# When passed a list of github repos, find out the ones I have never checked and the ones that I have checked in the past, but the last time was more than N months ago.
 # Those are the ones that I should check, so I should open them in the browser.
 # I should create/update them in buku and set their timestamp to today.
 # Cases:
 # - never checked -> insert them w/date to today, open in browser
-# - already checked, less than 3 months ago -> nothing
-# - already checked, more than 3 months ago -> update them w/date to today, open in browser
+# - already checked, less than N months ago -> nothing
+# - already checked, more than N months ago -> update them w/date to today, open in browser
 
-insert_all_repos () {
-    for url in $(cat $repos_file)
-    do
-        buku --add "$url" github trending -c checked "$(today)" >&/dev/null
+insert_all_repos() {
+    for url in $(cat $repos_file); do
+        buku --add "$url" github trending -c checked "$(today)" >/dev/null
     done
 }
 
-update_and_open_stale_repo () {
+update_and_open_stale_repo() {
     local url="$1"
 
     local buku_id=$(buku --json --sreg "^$url$" | jq --raw-output '.[0].index')
@@ -24,46 +25,43 @@ update_and_open_stale_repo () {
     open_repo $url
 }
 
-open_repo () {
+open_repo() {
     local url="$1"
 
-    chromium $url >&/dev/null
+    chromium $url >&/dev/null &
 }
 
-is_description_format_wrong () {
+is_description_format_wrong() {
     local description="$1"
 
     echo $description | grep -qv "^checked [0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}$"
 }
 
-update_and_open_stale_repos () {
-    for url in $(cat $repos_file)
-    do
+update_and_open_stale_repos() {
+    for url in $(cat $repos_file); do
         local description=$(buku --json --sreg "^$url$" | jq --raw-output '.[0].description')
         local last_update_date=$(echo "$description" | cut -d' ' -f2)
-        if [ "$last_update_date" == $(today) ]
-        then
+        if [ "$last_update_date" == $(today) ]; then
             open_repo "$url"
-        elif is_description_format_wrong "$description" || [[ $last_update_date < $(three_months_ago) ]]
-        then
+        elif is_description_format_wrong "$description" || [[ $last_update_date < $(months_ago 4) ]]; then
             update_and_open_stale_repo "$url"
         fi
     done
 }
 
-check_github_trending_repos () {
+check_github_trending_repos() {
     local repos_file="$1"
 
     insert_all_repos
     update_and_open_stale_repos
 }
 
-today () {
+today() {
     date +"%Y-%m-%d"
 }
 
-three_months_ago () {
-    date -d "-3 month" +"%Y-%m-%d"
+months_ago() {
+    date -d "-${1} month" +"%Y-%m-%d"
 }
 
 check_github_trending_repos urls.txt
